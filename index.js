650bdc01dac6c9a1221ae8588fbda4e3575fe99b
Danielhk832 <aox.soljah@gmail.com>
2021-08-13 21:13:44 +0700

    made small change for codegrade setup

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
+function removeArtist(/*Your Code */) {
   /*Your Code Here*/
 }
 
